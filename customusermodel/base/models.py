4fa2711d9bf1b41d5bd558064ae89f21c57a5108
전은형 <ooooo5h91@gmail.com>
2022-03-04 18:11:16 +0900

    로그인시 email로 로그인하도록 커스터마이징함

--- customusermodel/base/models.py
+++ customusermodel/base/models.py
@@ -4,4 +4,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(null=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
